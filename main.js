c92c1e3b58b00904b8b5b60a5b7dbb4488323fc7
tom <lim1017@hotmail.com>
2024-02-12 22:31:20 -0500

    added first threejs sphere

---
+++ main.js
@@ -0,0 +1,31 @@
+import * as THREE from "three";
+import "./style.css";
+
+const scene = new THREE.Scene();
+
+// Creating sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({ color: "#00ff83" });
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+const light = new THREE.DirectionalLight(0xffffff, 1, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
+camera.position.z = 20;
+scene.add(camera);
+
+// Render
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
